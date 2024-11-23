faa0db26f84aad702d213bc77f6d80fa2134f754
Subed <subedshahxi@gmail.com>
2024-11-23 23:14:01 +0545

    props applied

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import "./style.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar 
+        photoName='IMG_2166.JPG'
+        name='my pic' />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+        
+      </div>
     </div>
   );
 }
+function Avatar(props){
+  return(
+    <img 
+   className='avatar' src={props.photoName} alt={props.name} />
+  );
+}
+
+function Intro(){
+  return(
+    <div className="intro">
+      <h1>Subed Shah</h1>
+      <p>a software engineer who can work on both the front-end and back-end of a website or application. </p>
+    </div>
+  )
+}
+
+function SkillList(){
+  return(
+    <div className="skill-list">
+      <Skill skill='react' emoji='🤷‍♂️' color='red' />
+      <Skill skill='node.js' emoji='🤷🛒' color='blue' />
+    </div>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className="skill" style={{backgroundColor: props.color}}>
+     <span>{props.skill}</span>  
+     <span>{props.emoji}</span>  
+    </div>
+  )
+}
+
+
 
 export default App;
