c6714c47e9b270adb2ebac26ef92ca844ac0181a
Subed <subedshahxi@gmail.com>
2024-11-30 11:12:16 +0545

    created state variable

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,26 +1,32 @@
+import {useState} from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-function App(){
-  const step = 1;
+export default function App(){
+  const [step, setStep] = useState(1);
 
   function handlePrevious(){
-    alert("previous");
+    if(step > 1)
+      {
+        setStep (step - 1);
+      }
   }
 
   function handleNext(){
-    alert("Next");
+    if(step < 3){
+    setStep(step + 1);
+    }
   }
 
   return (
   <div className="steps">
      <div className="numbers">
-        <div className={`${step >= 1? "active" : ""}`}>1</div>
-        <div className={`${step >= 2? "active" : ""}`}>2</div>
-        <div className={`${step >= 3? "active" : ""}`}>3</div>
+        <div className={step >= 1? "active" : ""}>1</div>
+        <div className={step >= 2? "active" : ""}>2</div>
+        <div className={step >= 3? "active" : ""}>3</div>
      </div>
       <p className="message">
         Step {step}: {messages[step -1]}
@@ -41,5 +47,4 @@ function App(){
       </div>
   </div>
   );
-}
-export default App;
+};
