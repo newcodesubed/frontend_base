c0a3c294fca028ca64bb2318dec8dabc5541f888
Subed <subedshahxi@gmail.com>
2024-11-30 12:21:08 +0545

    feat: adding anothe state

--- steps/src/App.js
+++ steps/src/App.js
@@ -7,6 +7,8 @@ const messages = [
 
 export default function App(){
   const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true)
+  // const [test, setTest] = useState({name :"jonas"});
 
   function handlePrevious(){
     if(step > 1)
@@ -19,32 +21,44 @@ export default function App(){
     if(step < 3){
     setStep(step + 1);
     }
+    // setTest({name: "Subed"})
+
   }
 
   return (
-  <div className="steps">
-     <div className="numbers">
+<>
+      <button className="close" onClick={()=>setIsOpen(!isOpen)}>
+        &times;
+      </button>
+  {
+    isOpen && (
+    <div className="steps">
+      <div className="numbers">
         <div className={step >= 1? "active" : ""}>1</div>
         <div className={step >= 2? "active" : ""}>2</div>
         <div className={step >= 3? "active" : ""}>3</div>
-     </div>
-      <p className="message">
-        Step {step}: {messages[step -1]}
-      </p>
+      </div>
+        <p className="message">
+          Step {step}: {messages[step -1]}
+          {/* {test.name} */}
+        </p>
       <div className="buttons">
         <button 
           style={{backgroundColor: "#7950f2", color:"white"}}
           onClick={handlePrevious}
-        >
+          >
           Previous
         </button>
         <button 
           style={{backgroundColor: "#7950f2", color:"white"}}
           onClick={handleNext}
-        >
+          >
           Next
         </button>
       </div>
-  </div>
+    </div>
+    )
+  }     
+</>
   );
 };
