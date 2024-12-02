d060540e56802d5dd5ce6e50a8681db0d39004b4
Subed <subedshahxi@gmail.com>
2024-12-02 16:37:58 +0545

    feat: adding date when count is increased

--- counter/src/App.js
+++ counter/src/App.js
@@ -10,6 +10,9 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  var d = new Date();
+  d.setDate(d.getDate() + count);
+
   function addHandler() {
     setCount(count + 1);
   }
@@ -20,11 +23,20 @@ function Counter() {
   return (
     <>
       <div>
+        <button onClick={subHandler}>- </button>
+        Counter : {count}
         <button onClick={addHandler}>+</button>
-        <h1>Counter : {count}</h1>
-        <button onClick={subHandler}>-</button>
       </div>
-      <p></p>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{d.toDateString()}</span>
+      </p>
     </>
   );
 }
