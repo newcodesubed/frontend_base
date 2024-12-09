5fcda0908a03032b94aa6898399d3fcfad6c4d2d
Subed <subedshahxi@gmail.com>
2024-12-09 17:48:18 +0545

    refactor: use of Reusable Button

---
+++ steps/src/App-v1.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function Appv1() {
+  return (
+    <div>
+      <Steps />
+      {/* <Steps /> */}
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test, setTest] = useState({name :"jonas"});
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+    // setTest({name: "Subed"})
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+          <div className="buttons">
+            <Button
+              bgColor="#7950f2"
+              textColor="white"
+              onClick={handlePrevious}
+              text="Previous"
+            />
+            <Button
+              bgColor="#7950f2"
+              textColor="white"
+              onClick={handleNext}
+              text="Next"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, text }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+}
