44d3681ded48e276d6fd2745f4e7b34dd41b3823
Subed <subedshahxi@gmail.com>
2024-12-21 00:08:41 +0545

    feat: mark as complete

--- goaltracker/src/components/GList.jsx
+++ goaltracker/src/components/GList.jsx
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
 function Glist({ list }) {
+  const [isGreen, setIsGreen] = useState(false);
+  function handleClick() {
+    setIsGreen((curr) => !curr);
+  }
   return (
     <li>
-      <p>{list.goal}</p>
+      <p onClick={handleClick} style={{ color: isGreen ? "green" : "red" }}>
+        {list.goal}
+      </p>
     </li>
   );
 }
