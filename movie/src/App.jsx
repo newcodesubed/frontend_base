5be16652217e3ae7257a236a8d7ce01be703a7ac
Subed <subedshahxi@gmail.com>
2025-01-20 11:37:12 +0545

    feat: Changing page title

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -290,6 +290,13 @@ function MovieDetails({ selectedId, onCloseMovie }) {
     },
     [selectedId]
   );
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
   return (
     <div className="details">
       {isLoading ? (
