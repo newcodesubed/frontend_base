ebffd77a8f05f4ea8e8699447a5ff93b501f34ee
Subed <subedshahxi@gmail.com>
2025-01-21 18:58:46 +0545

    feat: converstion complete

--- moneyexchange/src/App.js
+++ moneyexchange/src/App.js
@@ -1,29 +1,61 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function App() {
-  const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState(10);
   const [fromAmount, setFromAmount] = useState("USD");
   const [toAmount, setToAmount] = useState("USD");
+  const [outputAmount, setOutputAmount] = useState(1);
+  // const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      async function fetchCurremcy() {
+        if (fromAmount === toAmount) {
+          setOutputAmount(amount);
+          return;
+        }
+        try {
+          const response = await fetch(
+            `https://api.frankfurter.app/latest?amount=1&from=${fromAmount}&to=${toAmount}`
+          );
+          if (!response.ok)
+            throw new Error("Something went wrong with fetching Rate");
+
+          const data = await response.json();
+          const rate = data.rates[toAmount];
+          if (!rate) throw new Error("Rate not found for selected currency");
+
+          const convertedAmount = (amount * rate)?.toFixed(2);
+          console.log(convertedAmount);
+          setOutputAmount(convertedAmount);
+        } catch (err) {
+          console.log(err.message);
+        }
+      }
+      fetchCurremcy();
+    },
+    [amount, fromAmount, toAmount]
+  );
   return (
     <div>
       <input
         type="text"
         value={amount}
-        onChange={(e) => setAmount(e.target.value)}
+        onChange={(e) => setAmount(e.target.value ? Number(e.target.value) : 0)}
       />
-      <select onClick={(e) => setFromAmount(e.target.value)}>
+      <select onChange={(e) => setFromAmount(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select onClick={(e) => setToAmount(e.target.value)}>
+      <select onChange={(e) => setToAmount(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{outputAmount} </p>
     </div>
   );
 }
