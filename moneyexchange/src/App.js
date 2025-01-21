52792d4f12cd9a47f7a07fdf3a5aee84a2c80746
Subed <subedshahxi@gmail.com>
2025-01-21 15:45:27 +0545

    adding statehook

--- moneyexchange/src/App.js
+++ moneyexchange/src/App.js
@@ -1,16 +1,23 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
-
+import { useState } from "react";
 export default function App() {
+  const [amount, setAmount] = useState();
+  const [fromAmount, setFromAmount] = useState("USD");
+  const [toAmount, setToAmount] = useState("USD");
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <select onClick={(e) => setFromAmount(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select onClick={(e) => setToAmount(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
