572bab954dd2682c89729c02a460ae8a38070c18
Subed <subedshahxi@gmail.com>
2025-02-02 23:56:27 +0545

    first routing

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
 function App() {
-  return <div>WORLDWISE</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
