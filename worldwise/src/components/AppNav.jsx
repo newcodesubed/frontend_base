356f9f39d9f1e52880bbb986ab072f06bd1a75f7
Subed <subedshahxi@gmail.com>
2025-02-04 23:29:59 +0545

    use of css module and othe static file

---
+++ worldwise/src/components/AppNav.jsx
@@ -0,0 +1,6 @@
+import style from "./AppNav.module.css";
+function AppNav() {
+  return <nav className={style.nav}>App navigation</nav>;
+}
+
+export default AppNav;
---
+++ worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import App from "../App";
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <App />
+    </div>
+  );
+}
+
+export default AppLayout;
