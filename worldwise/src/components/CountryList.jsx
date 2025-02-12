2175b6813a4f232b3aa8d6a782b2dd66bae0d47f
Subed <subedshahxi@gmail.com>
2025-02-13 00:10:44 +0545

    feat: adding the country only once

--- worldwise/src/components/CountryList.jsx
+++ worldwise/src/components/CountryList.jsx
@@ -1,7 +1,8 @@
 import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
-import CityItem from "./CityItem";
+
 import Message from "./Message";
+import CountryItem from "./CountryItem";
 function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
@@ -9,10 +10,16 @@ function CountryList({ cities, isLoading }) {
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
   return (
     <ul className={styles.countryList}>
-      {cities.map((city) => (
-        <CityItem key={city.id} city={city} />
+      {countries.map((country) => (
+        <CountryItem country={country} />
       ))}
     </ul>
   );
