a74e82267db0c3058af2ed4adf1d5b7a15222f33
Subed <subedshahxi@gmail.com>
2025-02-14 13:15:13 +0545

    feat: Read ing and setting a query string

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -8,10 +8,14 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date, id } = city;
+  const { cityName, emoji, date, id, position } = city;
+  //url ma cahi City bata haldai xa position ma handai ho ani url bata sabai le access gardaixa
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)}) </time>
