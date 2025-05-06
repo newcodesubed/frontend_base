61ed0653905b948ae327559f371805d520f3de6c
Subed <subedshahxi@gmail.com>
2025-05-07 01:33:41 +0545

    faet: Login

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -11,36 +11,39 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/fakeAuthContext";
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
 
-          <Route path="app" element={<AppLayout />}>
-            {/* default index feature */}
-            <Route index element={<Navigate replace to="cities" />} />
-            {/* can access with urls */}
+            <Route path="app" element={<AppLayout />}>
+              {/* default index feature */}
+              <Route index element={<Navigate replace to="cities" />} />
+              {/* can access with urls */}
 
-            {/* useCities context is used in them so passing it as global variable */}
-            <Route path="cities" element={<CityList />} />
+              {/* useCities context is used in them so passing it as global variable */}
+              <Route path="cities" element={<CityList />} />
 
-            <Route path="cities/:id" element={<City />} />
+              <Route path="cities/:id" element={<City />} />
 
-            {/* useCities context is used in them so passing it as global variable */}
-            <Route path="country" element={<CountryList />} />
+              {/* useCities context is used in them so passing it as global variable */}
+              <Route path="country" element={<CountryList />} />
 
-            <Route path="form" element={<Form />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   );
 }
 
--- worldwise/src/contexts/fakeAuthContext.jsx
+++ worldwise/src/contexts/fakeAuthContext.jsx
@@ -29,7 +29,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "subed",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
@@ -60,6 +60,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
