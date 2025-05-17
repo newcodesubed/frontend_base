82d0e4174b8ba17d09b2695f8fc85182411f7c84
Subed <subedshahxi@gmail.com>
2025-05-17 23:59:20 +0545

    feat: loading state

--- redux/src/features/accounts/accountSlice.js
+++ redux/src/features/accounts/accountSlice.js
@@ -2,11 +2,16 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
 
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
@@ -29,6 +34,9 @@ export default function accountReducer(state = initialStateAccount, action) {
         balance: state.balance - state.loan,
       };
 
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
+
     default:
       return state;
   }
@@ -38,6 +46,7 @@ export default function accountReducer(state = initialStateAccount, action) {
 export function deposit(amount, currency) {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
   return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
     //API call
 
     const res = await fetch(
