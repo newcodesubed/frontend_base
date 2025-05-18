fc2f532ea8940edc26c7c551f0e316c0430ede8f
Subed <subedshahxi@gmail.com>
2025-05-18 15:29:24 +0545

    feat: currency convertion

--- redux/src/features/accounts/AccountOperations.js
+++ redux/src/features/accounts/AccountOperations.js
@@ -21,10 +21,10 @@ function AccountOperations() {
   function handleDeposit() {
     if (!depositAmount) return;
 
-    // dispatch(deposit(depositAmount, currency));
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
+    // dispatch(deposit(depositAmount));
     setDepositAmount("");
-    setCurrency("");
+    setCurrency("USD");
   }
 
   function handleWithdrawal() {
