e80bd3e128fd3ab4c74e6f9f37348591c4ccdd92
Subed <subedshahxi@gmail.com>
2025-05-26 23:01:01 +0545

    styling form elements

--- fast-react-pizza/src/features/order/SearchOrder.jsx
+++ fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -14,9 +14,10 @@ function SearchOrder() {
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder="search you order#00000"
+        placeholder="search order #00000"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
