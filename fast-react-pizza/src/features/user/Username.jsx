5e02ab4fc73fa982980b5e8a121e974dbe93fd5e
Subed <subedshahxi@gmail.com>
2025-05-26 16:12:10 +0545

    using css grid

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Subed</div>;
+  return <div className="hidden text-sm font-semibold md:block">Subed</div>;
 }
 
 export default Username;
