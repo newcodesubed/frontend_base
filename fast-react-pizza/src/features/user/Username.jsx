fa71cd6b00c3164bb506906081dc13365721b01f
Subed <subedshahxi@gmail.com>
2025-05-26 13:33:53 +0545

    style: box model

--- fast-react-pizza/src/features/user/Username.jsx
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Subed</div>;
+  return <div className="hidden text-sm font-semibold">Subed</div>;
 }
 
 export default Username;
