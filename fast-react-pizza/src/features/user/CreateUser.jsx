6adafd74b982e65c6e55ca67a1b06861f207ad0b
Subed <subedshahxi@gmail.com>
2025-05-28 13:28:14 +0545

    feat: reading and updating USer state

--- fast-react-pizza/src/features/user/CreateUser.jsx
+++ fast-react-pizza/src/features/user/CreateUser.jsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import { useDispatch } from "react-redux";
 import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!username) return;
     // username is passed with userSLlice
     dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
