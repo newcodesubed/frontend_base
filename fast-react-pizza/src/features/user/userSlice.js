6090dc2bc23382b81c5adaba8ad8f10f378d0362
Subed <subedshahxi@gmail.com>
2025-05-28 12:29:01 +0545

    debug: reducer-> reducers for username

--- fast-react-pizza/src/features/user/userSlice.js
+++ fast-react-pizza/src/features/user/userSlice.js
@@ -25,13 +25,13 @@ async function fetchAddress() {
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  username: "alina",
+  username: "",
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducer: {
+  reducers: {
     updateName(state, action) {
       state.username = action.payload;
     },
