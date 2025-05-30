a7dbcc1af891f602dafd1db8fe20d8d785cb0033
Subed <subedshahxi@gmail.com>
2025-05-30 13:28:04 +0545

    feat: update data without navigation

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -6,6 +6,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderACtion } from "./features/order/UpdateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 //doing this way to load data
@@ -38,6 +39,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        action: updateOrderACtion,
         errorElement: <Error />,
       },
     ],
--- fast-react-pizza/src/features/order/Order.jsx
+++ fast-react-pizza/src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 function Order() {
   const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
@@ -85,6 +86,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
