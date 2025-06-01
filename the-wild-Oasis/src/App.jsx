348dcb018b44a205be1357754dded5122058e9cd
Subed <subedshahxi@gmail.com>
2025-06-01 22:48:40 +0545

    Building Reuseable style

--- the-wild-Oasis/src/App.jsx
+++ the-wild-Oasis/src/App.jsx
@@ -14,20 +14,28 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Row type="horizontal">
-          <Heading as="h1">The wild Oasis</Heading>
-          <div>
-            <Heading as="h2">Check in and Out</Heading>
-            <Button onClick={() => alert("check in")}>check In</Button>
-            <Button onClick={() => alert("check out")}>check Out</Button>
-          </div>
-        </Row>
-        <Row type="vertical">
-          <Heading as="h3">Form</Heading>
-          <form>
-            <Input type="number" placeholder="Number of guest" />
-            <Input type="number" placeholder="Number of guest" />
-          </form>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and Out</Heading>
+              <Button onClick={() => alert("check in")}>check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("check out")}
+              >
+                check Out
+              </Button>
+            </div>
+          </Row>
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guest" />
+              <Input type="number" placeholder="Number of guest" />
+            </form>
+          </Row>
         </Row>
       </StyledApp>
     </>
