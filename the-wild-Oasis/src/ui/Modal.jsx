a9ce746a9f8b2725b77e0cad6c68d0c923aa92f7
Subed <subedshahxi@gmail.com>
2025-06-27 11:09:59 +0545

    prevent overFlow property hidden by using createPortal which create portal as selected in dom tree

--- the-wild-Oasis/src/ui/Modal.jsx
+++ the-wild-Oasis/src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 
@@ -50,7 +51,7 @@ const Button = styled.button`
   }
 `;
 function Modal({ children, onClose }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
@@ -58,7 +59,8 @@ function Modal({ children, onClose }) {
         </Button>
         <div>{children}</div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
 
