c57c0e2e45409d78979bc113c94c217449bf9c55
Subed <subedshahxi@gmail.com>
2025-06-27 11:32:33 +0545

    create frame for api for modal

--- the-wild-Oasis/src/features/cabins/AddCabin.jsx
+++ the-wild-Oasis/src/features/cabins/AddCabin.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
+//create api for the modal
+
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add to cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      {/* <Modal.Open opens='table'>
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name='table'>
+        <CreateCabinForm />
+      </Modal.Window> */}
+    </Modal>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
