30baca0245e57a60bac66b64ee88fde4c03c8138
Subed <subedshahxi@gmail.com>
2025-07-14 18:06:55 +0545

    feat: bilding bookingTable

--- the-wild-Oasis/src/features/bookings/useBookings.js
+++ the-wild-Oasis/src/features/bookings/useBookings.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 
-export function useCabins() {
+export function useBookings() {
   const {
     isLoading,
     data: bookings,
