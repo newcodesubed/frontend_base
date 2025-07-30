b91b82d912e74b11adeec499982bfb0de19ec21a
Subed <subedshahxi@gmail.com>
2025-07-30 17:46:32 +0545

    refact: filter with useParam hooks

--- the-wild-Oasis/src/services/apiBookings.js
+++ the-wild-Oasis/src/services/apiBookings.js
@@ -1,13 +1,19 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
-    )
-    .eq("status", "unconfirmed");
+    );
+
+  //FILTER
+  if (filter !== null) {
+    query = query.eq(filter.field, filter.value);
+  }
+
+  const { data, error } = await query;
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
